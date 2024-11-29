217d748ddb3e78290b050c3d7669bc199ee4e6c8
TCseba <t.seba143@gmail.com>
2024-11-29 22:48:50 +0200

    Update 03-promises.js

--- src/03-promises.js
+++ src/03-promises.js
@@ -1,8 +1,35 @@
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay })
+      }
+      else {
+        reject({ position, delay })
+      }
+    }, delay)
+  })
 }
+  const form = document.querySelector('.form');
+const delayInput = form.querySelector('input[name="delay"]');
+const stepInput = form.querySelector('input[name="step"]');
+const amountInput = form.querySelector('input[name="amount"]');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const firstDelay = Number(delayInput.value);
+  const delayStep = Number(stepInput.value);
+  const amount = Number(amountInput.value);
+
+  for (let i = 0; i < amount; i++) {
+    const delay = firstDelay + i * delayStep;
+    createPromise(i + 1, delay)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  }
+});
